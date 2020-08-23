d9531e720a7ff96f432c7180b795df6e981efb3f
Soumita <soumita.ghosh@u.nus.edu>
2020-08-23 20:10:41 +0800

    Updated mongo import

--- bin/mongo_import_db.sh
+++ bin/mongo_import_db.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-/usr/bin/mongod &
-
-cd /multislide/db
-unzip '*.zip'
+cd $1
+unzip -o '*.zip'
 ls -l
 
 allfilenames=`ls ./*.json`
@@ -11,11 +9,13 @@ allfilenames=`ls ./*.json`
 for eachfile in $allfilenames;
 do
    filename=$(basename "$eachfile")
+   dbname=$(basename -s .json "$eachfile") 
+   echo $filename, $dbname
    if grep -q "HS_" <<< "$filename"; then
-  		mongoimport --db geneVocab_HomoSapiens --collection ${i/.json/} --file $i
+  		mongoimport --db geneVocab_HomoSapiens --drop --collection $dbname --file $filename
    fi
    if grep -q "MM_" <<< "$filename"; then
-  		mongoimport --db geneVocab_MusMusculus --collection ${i/.json/} --file $i
+  		mongoimport --db geneVocab_MusMusculus --drop --collection $dbname --file $filename
    fi
 
 done
